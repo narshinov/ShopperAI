2751e723c8de02335f416ff2d178bc515fc14a0d
Nikita Arshinov <narshinov01@gmail.com>
2023-11-21 03:00:27 +0300

    Initial commit

---
+++ ShopperAI/ShopperAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopperAIApp.swift
+//  ShopperAI
+//
+//  Created by Nikita Arshinov on 21.11.23.
+//
+
+import SwiftUI
+
+@main
+struct ShopperAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
